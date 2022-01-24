b747e213198910105349dfdcb71aba224de8bde4
Katie Pundt <kzpundt@gmail.com>
2022-01-24 15:38:53 -0800

    create AccountController
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

---
+++ MyTrailsClient/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using MyTrailsClient.Models;
+// using MyTrailsClient.ViewModels;
+
+// namespace MyTrailsClient.Controllers
+// {
+//   public class AccountController : Controller
+//   {
+//     private readonly MyTrailsClientContext _db;
+
+//     private readonly UserManager<ApplicationUser> _userManager;
+//     private readonly SignInManager<ApplicationUser> __signInManager;
+//   }
+// }
