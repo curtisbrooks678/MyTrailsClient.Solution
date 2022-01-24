7e0947a157b70a18ca857a4062c7ce27882436c0
Katie Pundt <kzpundt@gmail.com>
2022-01-24 09:27:32 -0800

    add startup files
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

---
+++ MyTrailsClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyTrailsClient.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
