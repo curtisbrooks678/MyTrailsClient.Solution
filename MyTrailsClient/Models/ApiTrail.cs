5fd0838a5df214b98165d3e4d01a27612d33ea56
Katie Pundt <kzpundt@gmail.com>
2022-01-25 10:54:43 -0800

    add Trail API model and helper
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

---
+++ MyTrailsClient/Models/ApiTrail.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MyTrailsClient.Models
+{
+  public class ApiTrail
+  {
+    public int ApiTrailId { get; set; }
+    public string Name { get; set; }
+    public double Length { get; set; }
+    public string Configuration { get; set; }
+    public int ElevationGain { get; set; }
+    public string Difficulty { get; set; }
+    public string FamilyFriendly { get; set; }
+    public double DistanceFromPdx { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Season { get; set; }
+
+    public string Status { get; set; }
+
+    public static List<ApiTrail> GetApiTrails()
+    {
+      var apiCallTask = ApiHelperTrail.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<ApiTrail> apiTrailList = JsonConvert.DeserializeObject<List<ApiTrail>>(jsonResponse.ToString());
+
+      return apiTrailList;
+    }
+
+    public static ApiTrail GetDetails(int id)
+    {
+      var apiCallTask = ApiHelperTrail.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      ApiTrail apiTrail = JsonConvert.DeserializeObject<ApiTrail>(jsonResponse.ToString());
+
+      return apiTrail;
+
+    }
+  }
+}
