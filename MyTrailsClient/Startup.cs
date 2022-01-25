e13efb68d34b21b8616f6106fe66966587a22000
Katie Pundt <kzpundt@gmail.com>
2022-01-25 14:22:59 -0800

    edit Startup to add Identity
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

--- MyTrailsClient/Startup.cs
+++ MyTrailsClient/Startup.cs
@@ -4,54 +4,72 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyTrailsClient.Models;
 
 namespace MyTrailsClient
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
+		public Startup(IWebHostEnvironment env)
 		{
-			Configuration = configuration;
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json");
+			Configuration = builder.Build();
 		}
 
-		public IConfiguration Configuration { get; }
+		public IConfigurationRoot Configuration { get; set; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
+			services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<MyTrailsClientContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<MyTrailsClientContext>()
+        .AddDefaultTokenProviders();
+      
+			// remove password requirements after testing
+      services.Configure<IdentityOptions>(options => {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+		public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseAuthentication();
+      app.UseRouting();
+      app.UseAuthorization();
+      
+      app.UseEndpoints(routes =>
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
 
-			app.UseRouting();
+      app.UseStaticFiles();
 
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-			});
-		}
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Error!");
+      });
+    }
 	}
 }
