217e15f6f2f3b78d55c1e8ff6f858636c98a80b5
Katie Pundt <kzpundt@gmail.com>
2022-01-25 10:49:26 -0800

    modify VisitEntry properties
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

--- MyTrailsClient/Models/VisitEntry.cs
+++ MyTrailsClient/Models/VisitEntry.cs
@@ -14,22 +14,13 @@ namespace MyTrailsClient.Models
     public int VisitEntryId { get; set; }
 
     public DateTime VisitDate { get; set; }
-
-    public double Latitude { get; set; }
-
-    public double Longitude { get; set; }
-
-    public string Notes { get; set; }
-
-    public string Bounty { get; set; }
-
-    public string PostComments { get; set; }
-
     public string Rating { get; set; }
 
-    public string Thoughts { get; set; }
-
     public string ActivityLevel { get; set; }
+    public string Bounty { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Notes { get; set; }
     public virtual ApplicationUser User { get; set; }
     public virtual ICollection<UserTrailVisitEntry> JoinEntities { get; set;}
   }
