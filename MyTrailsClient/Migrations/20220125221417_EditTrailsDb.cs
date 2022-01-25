022332da7eb67be3580ea3f0449133c5e278d388
Katie Pundt <kzpundt@gmail.com>
2022-01-25 14:23:34 -0800

    edit Trails DB model properties
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-Authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-Authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-Authored-by: Jeff Lai <pjefflai@gmail.com>

---
+++ MyTrailsClient/Migrations/20220125221417_EditTrailsDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyTrailsClient.Migrations
+{
+    public partial class EditTrailsDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
